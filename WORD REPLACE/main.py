df42971236b72915e80aab324ba5357be27cdec8
e\Ela Lino <87192835+LinoELa@users.noreply.github.com>
2023-04-09 10:09:43 +0200

    Add files via upload
    Replazar palabras en un texto o una horacion

---
+++ WORD REPLACE/main.py
@@ -0,0 +1,11 @@
+
+
+
+def replazar_palabra():
+    str = 'Hola Colegas, Soy Ela, estoy de Zaragoza '
+    word_to_replace = input('Inserta la palabra a cambiar : ')
+    word_replacement = input ('Introdizca la nueva palabra  : ')
+    print(str.replace(word_to_replace, word_replacement))
+
+
+replazar_palabra()
